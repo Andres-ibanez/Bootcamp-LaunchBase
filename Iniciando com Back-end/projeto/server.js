8a5116dd99e31a39435cbe9ad3e573bb61a3f026
Andres <cristian.ibane@aluno.unila.edu.br>
2020-05-23 00:12:50 -0300

    feat: utilizando dados dinamicos aplicacion de template engine (nunjucks) para crear layout con codigo repetido

--- Iniciando com Back-end/projeto/server.js
+++ Iniciando com Back-end/projeto/server.js
@@ -5,16 +5,20 @@ const server = express()  // iniciando el servidor
 
 server.use(express.static('public')) // configuracion de lectura de archivos estatico .css
 
-server.set('view engine', 'html')
+server.set('view engine', 'html') // setar template engine
 
 nunjucks.configure('views', {
     express: server
 })
 
-server.get('/', function(req, res){
-    return res.render('index')
+server.get('/', function (req, res) {
+    return res.render('index')  //llamada a la pagina index
 })
 
-server.listen(5000, function(){
+server.get('/classes', function (req, res) {
+    return res.render('classes')  //llamada a la pagina classes
+})
+
+server.listen(5000, function () {
     console.log('server is runing')
 })
