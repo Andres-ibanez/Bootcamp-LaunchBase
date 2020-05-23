193880c915ab361aab1aff7954dd4a0f2dbbff5a
Andres <cristian.ibane@aluno.unila.edu.br>
2020-05-23 00:44:13 -0300

    fix: Actualizando problema con extencion njk -njk extenção nunjucks -Adicionando arquivo data.js e importado no arquivo de classes.

--- Iniciando com Back-end/projeto/server.js
+++ Iniciando com Back-end/projeto/server.js
@@ -2,10 +2,11 @@ const express = require('express')
 const nunjucks = require('nunjucks')
 
 const server = express()  // iniciando el servidor
+const videos = require("./data") //Llamando los datos dentro de data
 
 server.use(express.static('public')) // configuracion de lectura de archivos estatico .css
 
-server.set('view engine', 'html') // setar template engine
+server.set('view engine', 'njk') // setar template engine njk nunjucks
 
 nunjucks.configure('views', {
     express: server
@@ -16,7 +17,7 @@ server.get('/', function (req, res) {
 })
 
 server.get('/classes', function (req, res) {
-    return res.render('classes')  //llamada a la pagina classes
+    return res.render('classes', { items: videos})  //llamada a la pagina classes
 })
 
 server.listen(5000, function () {
