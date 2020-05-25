46302a1e48e429eaca469dfce897a8289d9c7136
Andres <cristian.ibane@aluno.unila.edu.br>
2020-05-25 15:30:02 -0300

    feat: usando valores dinamicos. Atualizando dados da home com dados dinamicos.

--- Iniciando com Back-end/projeto/server.js
+++ Iniciando com Back-end/projeto/server.js
@@ -9,11 +9,24 @@ server.use(express.static('public')) // configuracion de lectura de archivos est
 server.set('view engine', 'njk') // setar template engine njk nunjucks
 
 nunjucks.configure('views', {
-    express: server
+    express: server,
+    autoescape: false
 })
 
 server.get('/', function (req, res) {
-    return res.render('index')  //llamada a la pagina index
+    const about = {
+        img_url: "https://avatars3.githubusercontent.com/u/6643122?s=460&v=4",
+        name:"Mayk Brito",
+        role: "Instrutor - Rocketseat",
+        description: 'Programador full-stack, focado em trazer o melhor ensino para iniciantes em programação. Colaborador da <a href="https://rocketseat.com.br" target="_blank">Rocketseat</a>',
+        links: [
+            { name: "Github", url: "https://github.com/maykbrito/"},
+            { name: "Twitter", url: "https://twitter.com/maykbrito/"},
+            { name: "Linkedin", url: "https://www.linkedin.com/in/maykbrito/"}
+        ]
+
+    }
+    return res.render('index', { about })  //llamada a la pagina index e envio de datos
 })
 
 server.get('/classes', function (req, res) {
